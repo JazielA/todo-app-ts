29fa4b04534668076dfd1d2b66e4c339344f56bb
JazielA <jaz.aguilera@duocuc.cl>
2025-01-21 23:24:24 -0300

    componente Todo teminado

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,9 +1,13 @@
+// importo el tipo ListOfTodos de types.d.ts para separarlo como declaraciones de tipos.
 import { type ListOfTodos } from "../types";
 
+//La interfaz Props define las props que el componente Todos va a recibir.
 interface Props {
   todos: ListOfTodos;
 }
-
+//aqui defino que las props que recibe el componente
+//Todos deben ser de tipo ListOfTodos
+//(valores de la interfaz Todo en types.d.ts)
 export const Todos: React.FC<Props> = ({ todos }) => {
   return (
     <ul>
--- src/types.d.ts
+++ src/types.d.ts
@@ -2,12 +2,15 @@
 // aseguran que los datos tengan la forma esperada.
 
 //Si se pasa un prop que no cumple con la 
-// interfaz Props, TypeScript lanza un error.
+// interfaz Props (en el componente Todos.jsx), TypeScript lanza un error.
 
+//Creamos el contrato de los props que se le pasan al componente
 export interface Todo {
     id: string;
     title: string;
     completed: boolean;
 }
 
+
+//se define un alias para el tipo de dato que se espera recibir
 export type ListOfTodos = Todo[];
